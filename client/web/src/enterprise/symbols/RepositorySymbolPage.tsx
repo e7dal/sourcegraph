fbbc3a55e348b8ee2da2762e320ed2801e26fca8
Quinn Slack <quinn@slack.org>
2020-12-27 21:28:44 -0800

    wip

--- client/web/src/enterprise/symbols/RepositorySymbolPage.tsx
+++ client/web/src/enterprise/symbols/RepositorySymbolPage.tsx
@@ -125,108 +125,73 @@ export const RepositorySymbolPage: React.FunctionComponent<Props> = ({
         ])
     )
 
-    const sections = useMemo<{ id: string; label: string }[]>(
-        () => [
-            { id: 'def', label: 'Definition' },
-            { id: 'doc', label: 'Documentation' },
-            { id: 'examples', label: 'Examples' },
-            { id: 'refs', label: 'References' },
-        ],
-        []
-    )
-
-    const [activeSection, setActiveSection] = useState<string>(sections[0].id)
-    const onActiveSectionUpdate = useCallback((element: HTMLElement) => setActiveSection(element.id), [])
-
     return (
-        <div className="w-100" style={{ overflow: 'auto' }} id="RepositorySymbolPage">
+        <div className="container" style={{ overflow: 'auto' }}>
             {data === null ? (
                 <p>Not found</p>
             ) : data === undefined ? (
                 <p>Loading...</p>
             ) : (
-                <div className="row no-gutters flex-row-reverse">
-                    <div className="col-md-2">
-                        <Scrollspy
-                            items={sections.map(({ id }) => id)}
-                            componentTag="ul"
-                            className="nav nav-pills flex-column mx-3 pt-3 sticky-top"
-                            currentClassName="active"
-                            rootEl="#RepositorySymbolPage"
-                            onUpdate={onActiveSectionUpdate}
-                        >
-                            {sections.map(({ id, label }) => (
-                                <li key={id} className="nav-item">
-                                    <Link className={`nav-link ${activeSection === id ? 'active' : ''}`} to={`#${id}`}>
-                                        {label}
-                                    </Link>
-                                </li>
-                            ))}
-                        </Scrollspy>
-                    </div>
-                    <div className="col-md-10">
-                        <style>{'.markdown pre code { font-size: 18px; }'}</style>
-                        <section id="doc">
-                            {data.hover?.markdown.text && (
-                                <Markdown
-                                    dangerousInnerHTML={renderMarkdown(
-                                        data.hover?.markdown.text.split('---', 2)[0]
-                                    ).replace(/<hr\s?\/?>/g, '')}
-                                    history={history}
-                                    className="m-3"
-                                />
-                            )}
-
-                            <ul className="list-unstyled nav nav-pills m-3 d-flex flex-wrap">
-                                <li className="nav-item ">
-                                    <Link to="TODO" className="nav-link">
-                                        Defined in <code>token.go</code>
-                                    </Link>
-                                </li>
-                                <li className="nav-item ">
-                                    <Link to="TODO" className="nav-link">
-                                        References
-                                    </Link>
-                                </li>
-                            </ul>
-
-                            {data.hover?.markdown.text && (
-                                <Markdown
-                                    dangerousInnerHTML={renderMarkdown(
-                                        data.hover?.markdown.text.split('---', 2)[1]
-                                    ).replace(/<hr\s?\/?>/g, '')}
-                                    history={history}
-                                    className="m-3"
-                                />
-                            )}
-                        </section>
-                        <section id="refs" className="mt-5">
-                            <h2 className="m-3 h4">Usage examples ({data.references.nodes.length})</h2>
-                            <style>
-                                {
-                                    'td.line { display: none; } .code-excerpt .code { padding-left: 0.25rem !important; } .result-container__header { display: none; } .result-container { border-bottom: solid 1px var(--border-color); }'
-                                }
-                            </style>
-                            <FileLocations
-                                location={location}
-                                locations={of(
-                                    data.references.nodes.map<Location>(reference => ({
-                                        uri: makeRepoURI({
-                                            repoName: reference.resource.repository.name,
-                                            commitID: reference.resource.commit.oid,
-                                            filePath: reference.resource.path,
-                                        }),
-                                        range: reference.range!,
-                                    }))
-                                )}
-                                icon={SourceRepositoryIcon}
-                                isLightTheme={false /* TODO(sqs) */}
-                                fetchHighlightedFileLineRanges={fetchHighlightedFileLineRanges}
-                                settingsCascade={settingsCascade}
-                                versionContext={undefined /* TODO(sqs) */}
+                <div>
+                    <style>{'.markdown pre code { font-size: 18px; } .markdown pre { margin-bottom: 0; }'}</style>
+                    <section id="doc">
+                        {data.hover?.markdown.text && (
+                            <Markdown
+                                dangerousInnerHTML={renderMarkdown(
+                                    data.hover?.markdown.text.split('---', 2)[0]
+                                ).replace(/<hr\s?\/?>/g, '')}
+                                history={history}
+                                className="mt-3 mx-3"
                             />
-                        </section>
-                    </div>
+                        )}
+
+                        <ul
+                            className="list-unstyled nav nav-pills d-flex flex-wrap justify-content-end"
+                            style={{ position: 'relative', marginTop: '-2.31rem', marginRight: '1.09rem' }}
+                        >
+                            <li className="nav-item">
+                                <Link to="TODO" className="nav-link btn btn-secondary">
+                                    Go to definition
+                                </Link>
+                            </li>
+                        </ul>
+
+                        {data.hover?.markdown.text && (
+                            <Markdown
+                                dangerousInnerHTML={renderMarkdown(
+                                    data.hover?.markdown.text.split('---', 2)[1]
+                                ).replace(/<hr\s?\/?>/g, '')}
+                                history={history}
+                                className="m-3 pt-3"
+                            />
+                        )}
+                    </section>
+                    <section id="refs" className="mt-5">
+                        <h2 className="mt-3 mx-3 mb-0 h4">Examples</h2>
+                        <style>
+                            {
+                                'td.line { display: none; } .code-excerpt .code { padding-left: 0.25rem !important; } .result-container__header { display: none; } .result-container { border: solid 1px var(--border-color) !important; border-width: 1px !important; margin: 1rem; }'
+                            }
+                        </style>
+                        <FileLocations
+                            location={location}
+                            locations={of(
+                                data.references.nodes.slice(1, 4).map<Location>(reference => ({
+                                    uri: makeRepoURI({
+                                        repoName: reference.resource.repository.name,
+                                        commitID: reference.resource.commit.oid,
+                                        filePath: reference.resource.path,
+                                    }),
+                                    range: reference.range!,
+                                }))
+                            )}
+                            icon={SourceRepositoryIcon}
+                            isLightTheme={false /* TODO(sqs) */}
+                            fetchHighlightedFileLineRanges={fetchHighlightedFileLineRanges}
+                            settingsCascade={settingsCascade}
+                            versionContext={undefined /* TODO(sqs) */}
+                        />
+                    </section>
                 </div>
             )}
         </div>
